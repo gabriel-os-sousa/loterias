0a7010677402ea59b5b5aaf7f8fb2392674fb9ee
Gabriel Sousa <gabrielsousa.ti@gmail.com>
2023-09-30 12:18:04 -0300

    feat: initial commit
    (cherry picked from commit 5ccc8c9a9df48e7e2d3e075d89eaff9af0e8dbf5)

---
+++ src/main/java/com/gabriel_os_sousa/loterias/LoteriasApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel_os_sousa.loterias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoteriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel_os_sousa/loterias/LoteriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_os_sousa.loterias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
